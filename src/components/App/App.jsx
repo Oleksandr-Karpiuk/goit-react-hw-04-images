f752ac2047a62d2ca55817610a7947b65967f40d
Oleksandr Karpiuk <alex.karpuyk@gmail.com>
2023-05-12 15:10:41 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { fetchImages } from 'services/api';
@@ -9,121 +9,102 @@ import Button from 'components/Button';
 import ImageGallery from 'components/ImageGallery';
 import Modal from 'components/Modal';
 
-export default class App extends Component {
-  state = {
-    searchData: '',
-    images: [],
-    page: 1,
-    largeImage: '',
-    showModal: false,
-    isLoading: false,
-  };
+export default function App() {
+  const [searchData, setSearchData] = useState('');
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [largeImage, setLargeImage] = useState('');
+  const [showModal, setShowModal] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
-  async componentDidUpdate(prevProps, prevState) {
-    const prevPage = prevState.page;
-    const prevSearchData = prevState.searchData;
-    const { searchData, page, images } = this.state;
-    if (prevPage !== page || prevSearchData !== searchData) {
-      try {
-        this.setState({ isLoading: true });
-        const data = await fetchImages(searchData, page);
-        if (data.hits.length === 0) {
-          this.setState({ images: [] });
-          return toast.error('Nothing found');
-        }
-
-        const newData = data.hits.reduce(
-          (acc, { id, webformatURL, largeImageURL }) => {
-            if (images.filter(image => image.id === id).length === 0) {
-              acc.push({ id, webformatURL, largeImageURL });
-            }
-            return acc;
-          },
-          []
-        );
-
-        if (newData.length > 0) {
-          this.setState(({ images }) => ({
-            images: [...images, ...newData],
-          }));
-        }
-      } catch (error) {
-        toast.error(error.message);
-      } finally {
-        this.setState({ isLoading: false });
-      }
-    }
-
-    if (page > 1) {
-      const CARD_HEIGHT = 260;
-      window.scrollBy({
-        top: CARD_HEIGHT * 4,
-        behavior: 'smooth',
-      });
-    }
-  }
-
-  onSubmit = searchData => {
+  useEffect(() => {
     if (searchData.trim() === '') {
-      this.setState({ images: [] });
-      return toast.error('Enter the meaning for search');
-    } else if (searchData === this.state.searchData) {
+      setImages([]);
       return;
     }
 
-    this.setState({
-      searchData: searchData,
-      page: 1,
-      images: [],
-    });
+    fetchData();
+  }, [searchData, page]);
+
+  const fetchData = async () => {
+    try {
+      setIsLoading(true);
+      const data = await fetchImages(searchData, page);
+      if (data.hits.length === 0) {
+        setImages([]);
+        return toast.error('Nothing found');
+      }
+
+      const newData = data.hits.reduce(
+        (acc, { id, webformatURL, largeImageURL }) => {
+          if (images.filter(image => image.id === id).length === 0) {
+            acc.push({ id, webformatURL, largeImageURL });
+          }
+          return acc;
+        },
+        []
+      );
+
+      if (newData.length > 0) {
+        setImages(prevImages => [...prevImages, ...newData]);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      setIsLoading(false);
+    }
   };
 
-  nextPage = () => {
-    this.setState(({ page }) => ({ page: page + 1 }));
+  const onSubmit = searchData => {
+    if (searchData.trim() === '') {
+      setImages([]);
+      return toast.error('Enter the meaning for search');
+    } else if (!searchData) {
+      return;
+    }
+
+    setSearchData(searchData);
+    setPage(1);
+    setImages([]);
   };
 
-  openModal = index => {
-    this.setState(({ images }) => ({
-      showModal: true,
-      largeImage: images[index].largeImageURL,
-    }));
+  const nextPage = () => {
+    setPage(prevPage => prevPage + 1);
   };
 
-  toggleModal = () => {
-    this.setState(({ showModal }) => ({ showModal: !showModal }));
+  const openModal = index => {
+    setShowModal(true);
+    setLargeImage(images[index].largeImageURL);
   };
 
-  render() {
-    const { toggleModal, openModal, nextPage, onSubmit } = this;
-    const { images, isLoading, largeImage, showModal } = this.state;
+  const toggleModal = () => {
+    setShowModal(prevShowModal => !prevShowModal);
+  };
 
-    return (
-      <div className={css.app}>
-        <Searchbar onSubmit={onSubmit} />
-        {images.length !== 0 && (
-          <ImageGallery images={images} openModal={openModal} />
-        )}
-        {showModal && (
-          <Modal toggleModal={toggleModal} largeImage={largeImage} />
-        )}
-        {isLoading && (
-          <Dna
-            visible={true}
-            height="200"
-            width="200"
-            ariaLabel="dna-loading"
-            wrapperStyle={{
-              position: 'fixed',
-              top: '50%',
-              left: '50%',
-              transform: 'translate(-50%, -50%)',
-            }}
-            wrapperClass="dna-wrapper"
-          />
-        )}
-        <ToastContainer autoClose={2500} />
-        {images.length >= 12 && <Button nextPage={nextPage} />}
-      </div>
-    );
-  }
+  return (
+    <div className={css.app}>
+      <Searchbar onSubmit={onSubmit} />
+      {images.length !== 0 && (
+        <ImageGallery images={images} openModal={openModal} />
+      )}
+      {showModal && <Modal toggleModal={toggleModal} largeImage={largeImage} />}
+      {isLoading && (
+        <Dna
+          visible={true}
+          height="200"
+          width="200"
+          ariaLabel="dna-loading"
+          wrapperStyle={{
+            position: 'fixed',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+          }}
+          wrapperClass="dna-wrapper"
+        />
+      )}
+      <ToastContainer autoClose={2500} />
+      {images.length >= 12 && <Button nextPage={nextPage} />}
+    </div>
+  );
 }
